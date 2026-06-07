a92b8368e96f02869251e6b7e0fafd2aa55732e6
Ajay Ghodake <it.ajayghodake@gmail.com>
2026-06-07 21:17:37 +0530

    backend Hosted

--- frontend/src/services/expenses.js
+++ frontend/src/services/expenses.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// const API = axios.create({
+//     baseURL: 'http://localhost:5000/api',
+// });
+
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://expense-tracker-api-n7vc.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
